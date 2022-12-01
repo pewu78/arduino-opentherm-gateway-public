690abcd8f99999ef2e698636fcbfe07cdcaec584
pewu78 <pewu78@gmail.com>
2022-12-01 21:04:24 +0100

    Disable overrides on disconnection, restart ESP after 10 WIFI_DISCONNECTED events

--- src/gateway.cpp
+++ src/gateway.cpp
@@ -18,6 +18,8 @@ OT_CONTROLLER openthermCtrl(&state, THERMOSTAT_IN, THERMOSTAT_OUT, BOILER_IN, BO
 HomieNode thermostat("thermostat", "Thermostat", "thermostat", false, 0, 0, thermostatInputHandler);
 HomieNode boiler("boiler", "Boiler", "boiler");
 
+byte disconnected_count;
+
 void setup()
 {
   // Setup serial
@@ -25,6 +27,8 @@ void setup()
   Serial << endl
          << endl;
   // Serial.setDebugOutput(true); // ESP debug output
+
+  disconnected_count = 0;
 
   Log.begin(LOGGING_LEVEL, &Serial, false);
   Log.notice("Firmware: %s Version: %s" CR, firmwareName, firmwareVersion);
@@ -36,7 +40,7 @@ void setup()
 
   // Homie.setGlobalInputHandler(globalInputHandler);
   // Homie.setLoopFunction(homieLoopHandler);
-  
+
   thermostat.advertise("ch").setName("Central Heating").setDatatype("boolean").settable();
   thermostat.advertise("choverride").setName("Central Heating Override").setDatatype("boolean").settable();
   thermostat.advertise("dhw").setName("Domestic Hot Water").setDatatype("boolean").settable();
@@ -163,7 +167,7 @@ void sendState(HomieNode &node, const String &property, const T &state, bool &st
 {
   if (stateChg)
   {
-    Log.trace("[MQTT OUT][%s][%s][%s] ... ", strcmp(node.getName(),"Boiler") == 0 ? "B" : "T", property.c_str(), toHomiePayload(state));
+    Log.trace("[MQTT OUT][%s][%s][%s] ... ", strcmp(node.getName(), "Boiler") == 0 ? "B" : "T", property.c_str(), toHomiePayload(state));
     if (node.setProperty(property).send(toHomiePayload(state)) != 0)
     {
       stateChg = false; // clear changed flag on successfull send
@@ -207,118 +211,134 @@ void sendState()
   }
 }
 
-void onHomieEvent(const HomieEvent& event) {
-  switch(event.type) {
-    case HomieEventType::STANDALONE_MODE:
-      // Do whatever you want when standalone mode is started
-      Log.trace("HomieEvent:STANDALONE_MODE" CR);
-      break;
-    case HomieEventType::CONFIGURATION_MODE:
-      // Do whatever you want when configuration mode is started
-      Log.trace("HomieEvent:CONFIGURATION_MODE" CR);
-      break;
-    case HomieEventType::NORMAL_MODE:
-      // Do whatever you want when normal mode is started
-      Log.trace("HomieEvent:NORMAL_MODE" CR);
-      break;
-    case HomieEventType::OTA_STARTED:
-      // Do whatever you want when OTA is started
-      Log.trace("HomieEvent:OTA_STARTED" CR);
-      tOpenTherm.disable();
-      tSendState.disable();
-      break;
-    case HomieEventType::OTA_PROGRESS:
-      // Do whatever you want when OTA is in progress
-      // You can use event.sizeDone and event.sizeTotal
-      break;
-    case HomieEventType::OTA_FAILED:
-      // Do whatever you want when OTA is failed
-      Log.trace("HomieEvent:OTA_FAILED" CR);
-      tOpenTherm.enable();
-      tSendState.enable();
-      break;
-    case HomieEventType::OTA_SUCCESSFUL:
-      // Do whatever you want when OTA is successful
-      Log.trace("HomieEvent:OTA_SUCCESSFUL" CR);
-      tOpenTherm.enable();
-      tSendState.enable();
-      break;
-    case HomieEventType::ABOUT_TO_RESET:
-      // Do whatever you want when the device is about to reset
-      Log.trace("HomieEvent:ABOUT_TO_RESET" CR);
-      break;
-    case HomieEventType::WIFI_CONNECTED:
-      // Do whatever you want when Wi-Fi is connected in normal mode
-      // You can use event.ip, event.gateway, event.mask
-      Log.trace("HomieEvent:WIFI_CONNECTED" CR);
-      break;
-    case HomieEventType::WIFI_DISCONNECTED:
-      // Do whatever you want when Wi-Fi is disconnected in normal mode
-      // You can use event.wifiReason
-      /*
-        Wi-Fi Reason (souce: https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFi/examples/WiFiClientEvents/WiFiClientEvents.ino)
-        0  SYSTEM_EVENT_WIFI_READY               < ESP32 WiFi ready
-        1  SYSTEM_EVENT_SCAN_DONE                < ESP32 finish scanning AP
-        2  SYSTEM_EVENT_STA_START                < ESP32 station start
-        3  SYSTEM_EVENT_STA_STOP                 < ESP32 station stop
-        4  SYSTEM_EVENT_STA_CONNECTED            < ESP32 station connected to AP
-        5  SYSTEM_EVENT_STA_DISCONNECTED         < ESP32 station disconnected from AP
-        6  SYSTEM_EVENT_STA_AUTHMODE_CHANGE      < the auth mode of AP connected by ESP32 station changed
-        7  SYSTEM_EVENT_STA_GOT_IP               < ESP32 station got IP from connected AP
-        8  SYSTEM_EVENT_STA_LOST_IP              < ESP32 station lost IP and the IP is reset to 0
-        9  SYSTEM_EVENT_STA_WPS_ER_SUCCESS       < ESP32 station wps succeeds in enrollee mode
-        10 SYSTEM_EVENT_STA_WPS_ER_FAILED        < ESP32 station wps fails in enrollee mode
-        11 SYSTEM_EVENT_STA_WPS_ER_TIMEOUT       < ESP32 station wps timeout in enrollee mode
-        12 SYSTEM_EVENT_STA_WPS_ER_PIN           < ESP32 station wps pin code in enrollee mode
-        13 SYSTEM_EVENT_AP_START                 < ESP32 soft-AP start
-        14 SYSTEM_EVENT_AP_STOP                  < ESP32 soft-AP stop
-        15 SYSTEM_EVENT_AP_STACONNECTED          < a station connected to ESP32 soft-AP
-        16 SYSTEM_EVENT_AP_STADISCONNECTED       < a station disconnected from ESP32 soft-AP
-        17 SYSTEM_EVENT_AP_STAIPASSIGNED         < ESP32 soft-AP assign an IP to a connected station
-        18 SYSTEM_EVENT_AP_PROBEREQRECVED        < Receive probe request packet in soft-AP interface
-        19 SYSTEM_EVENT_GOT_IP6                  < ESP32 station or ap or ethernet interface v6IP addr is preferred
-        20 SYSTEM_EVENT_ETH_START                < ESP32 ethernet start
-        21 SYSTEM_EVENT_ETH_STOP                 < ESP32 ethernet stop
-        22 SYSTEM_EVENT_ETH_CONNECTED            < ESP32 ethernet phy link up
-        23 SYSTEM_EVENT_ETH_DISCONNECTED         < ESP32 ethernet phy link down
-        24 SYSTEM_EVENT_ETH_GOT_IP               < ESP32 ethernet got IP from connected AP
-        25 SYSTEM_EVENT_MAX
-      */
-      Log.trace("HomieEvent:WIFI_DISCONNECTED Reason:%i" CR, event.wifiReason);
-      break;
-    case HomieEventType::MQTT_READY:
-      // Do whatever you want when MQTT is connected in normal mode
-      Log.trace("HomieEvent:MQTT_READY" CR);
-      tSendState.enable();
-      break;
-    case HomieEventType::MQTT_DISCONNECTED:
-      // Do whatever you want when MQTT is disconnected in normal mode
-      // You can use event.mqttReason
-      /*
-        MQTT Reason (source: https://github.com/marvinroger/async-mqtt-client/blob/master/src/AsyncMqttClient/DisconnectReasons.hpp)
-        0 TCP_DISCONNECTED
-        1 MQTT_UNACCEPTABLE_PROTOCOL_VERSION
-        2 MQTT_IDENTIFIER_REJECTED
-        3 MQTT_SERVER_UNAVAILABLE
-        4 MQTT_MALFORMED_CREDENTIALS
-        5 MQTT_NOT_AUTHORIZED
-        6 ESP8266_NOT_ENOUGH_SPACE
-        7 TLS_BAD_FINGERPRINT
-      */
-      Log.trace("HomieEvent:MQTT_DISCONNECTED Reason:%i" CR, event.mqttReason);
-      tSendState.disable();
-      break;
-    case HomieEventType::MQTT_PACKET_ACKNOWLEDGED:
-      // Do whatever you want when an MQTT packet with QoS > 0 is acknowledged by the broker
-      // You can use event.packetId
-      break;
-    case HomieEventType::READY_TO_SLEEP:
-      // After you've called `prepareToSleep()`, the event is triggered when MQTT is disconnected
-      Log.trace("HomieEvent:READY_TO_SLEEP" CR);
-      break;
-    case HomieEventType::SENDING_STATISTICS:
-      // Do whatever you want when statistics are sent in normal mode
-      Log.trace("HomieEvent:SENDING_STATISTICS" CR);
-      break;
+void onHomieEvent(const HomieEvent &event)
+{
+  switch (event.type)
+  {
+  case HomieEventType::STANDALONE_MODE:
+    // Do whatever you want when standalone mode is started
+    Log.trace("HomieEvent:STANDALONE_MODE" CR);
+    break;
+  case HomieEventType::CONFIGURATION_MODE:
+    // Do whatever you want when configuration mode is started
+    Log.trace("HomieEvent:CONFIGURATION_MODE" CR);
+    break;
+  case HomieEventType::NORMAL_MODE:
+    // Do whatever you want when normal mode is started
+    Log.trace("HomieEvent:NORMAL_MODE" CR);
+    break;
+  case HomieEventType::OTA_STARTED:
+    // Do whatever you want when OTA is started
+    Log.trace("HomieEvent:OTA_STARTED" CR);
+    tOpenTherm.disable();
+    tSendState.disable();
+    break;
+  case HomieEventType::OTA_PROGRESS:
+    // Do whatever you want when OTA is in progress
+    // You can use event.sizeDone and event.sizeTotal
+    break;
+  case HomieEventType::OTA_FAILED:
+    // Do whatever you want when OTA is failed
+    Log.trace("HomieEvent:OTA_FAILED" CR);
+    tOpenTherm.enable();
+    tSendState.enable();
+    break;
+  case HomieEventType::OTA_SUCCESSFUL:
+    // Do whatever you want when OTA is successful
+    Log.trace("HomieEvent:OTA_SUCCESSFUL" CR);
+    tOpenTherm.enable();
+    tSendState.enable();
+    break;
+  case HomieEventType::ABOUT_TO_RESET:
+    // Do whatever you want when the device is about to reset
+    Log.trace("HomieEvent:ABOUT_TO_RESET" CR);
+    break;
+  case HomieEventType::WIFI_CONNECTED:
+    // Do whatever you want when Wi-Fi is connected in normal mode
+    // You can use event.ip, event.gateway, event.mask
+    Log.trace("HomieEvent:WIFI_CONNECTED" CR);
+    disconnected_count = 0;
+    break;
+  case HomieEventType::WIFI_DISCONNECTED:
+    // Do whatever you want when Wi-Fi is disconnected in normal mode
+    // You can use event.wifiReason
+    /*
+      WIFI_REASON_UNSPECIFIED              = 1,
+      WIFI_REASON_AUTH_EXPIRE              = 2,
+      WIFI_REASON_AUTH_LEAVE               = 3,
+      WIFI_REASON_ASSOC_EXPIRE             = 4,
+      WIFI_REASON_ASSOC_TOOMANY            = 5,
+      WIFI_REASON_NOT_AUTHED               = 6,
+      WIFI_REASON_NOT_ASSOCED              = 7,
+      WIFI_REASON_ASSOC_LEAVE              = 8,
+      WIFI_REASON_ASSOC_NOT_AUTHED         = 9,
+      WIFI_REASON_DISASSOC_PWRCAP_BAD      = 10,
+      WIFI_REASON_DISASSOC_SUPCHAN_BAD     = 11,
+      WIFI_REASON_IE_INVALID               = 13,
+      WIFI_REASON_MIC_FAILURE              = 14,
+      WIFI_REASON_4WAY_HANDSHAKE_TIMEOUT   = 15,
+      WIFI_REASON_GROUP_KEY_UPDATE_TIMEOUT = 16,
+      WIFI_REASON_IE_IN_4WAY_DIFFERS       = 17,
+      WIFI_REASON_GROUP_CIPHER_INVALID     = 18,
+      WIFI_REASON_PAIRWISE_CIPHER_INVALID  = 19,
+      WIFI_REASON_AKMP_INVALID             = 20,
+      WIFI_REASON_UNSUPP_RSN_IE_VERSION    = 21,
+      WIFI_REASON_INVALID_RSN_IE_CAP       = 22,
+      WIFI_REASON_802_1X_AUTH_FAILED       = 23,
+      WIFI_REASON_CIPHER_SUITE_REJECTED    = 24,
+
+      WIFI_REASON_BEACON_TIMEOUT           = 200,
+      WIFI_REASON_NO_AP_FOUND              = 201,
+      WIFI_REASON_AUTH_FAIL                = 202,
+      WIFI_REASON_ASSOC_FAIL               = 203,
+      WIFI_REASON_HANDSHAKE_TIMEOUT        = 204,
+    */
+    Log.trace("HomieEvent:WIFI_DISCONNECTED Reason:%i" CR, event.wifiReason);
+    disconnected_count++;
+    Log.trace("Disconnected count:%i" CR, disconnected_count);
+    if (disconnected_count == 1)
+    {
+      state.thermostat.chOverride = false;
+      state.thermostat.dhwOverride = false;
+    }
+    if (disconnected_count == 10)
+    {
+      ESP.restart();
+    }
+    break;
+  case HomieEventType::MQTT_READY:
+    // Do whatever you want when MQTT is connected in normal mode
+    Log.trace("HomieEvent:MQTT_READY" CR);
+    tSendState.enable();
+    break;
+  case HomieEventType::MQTT_DISCONNECTED:
+    // Do whatever you want when MQTT is disconnected in normal mode
+    // You can use event.mqttReason
+    /*
+      MQTT Reason (source: https://github.com/marvinroger/async-mqtt-client/blob/master/src/AsyncMqttClient/DisconnectReasons.hpp)
+      0 TCP_DISCONNECTED
+      1 MQTT_UNACCEPTABLE_PROTOCOL_VERSION
+      2 MQTT_IDENTIFIER_REJECTED
+      3 MQTT_SERVER_UNAVAILABLE
+      4 MQTT_MALFORMED_CREDENTIALS
+      5 MQTT_NOT_AUTHORIZED
+      6 ESP8266_NOT_ENOUGH_SPACE
+      7 TLS_BAD_FINGERPRINT
+    */
+    Log.trace("HomieEvent:MQTT_DISCONNECTED Reason:%i" CR, event.mqttReason);
+    tSendState.disable();
+    break;
+  case HomieEventType::MQTT_PACKET_ACKNOWLEDGED:
+    // Do whatever you want when an MQTT packet with QoS > 0 is acknowledged by the broker
+    // You can use event.packetId
+    break;
+  case HomieEventType::READY_TO_SLEEP:
+    // After you've called `prepareToSleep()`, the event is triggered when MQTT is disconnected
+    Log.trace("HomieEvent:READY_TO_SLEEP" CR);
+    break;
+  case HomieEventType::SENDING_STATISTICS:
+    // Do whatever you want when statistics are sent in normal mode
+    Log.trace("HomieEvent:SENDING_STATISTICS" CR);
+    break;
   }
 }
